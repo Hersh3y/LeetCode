36c0fc5a7ce5fdbc816bcd167d9968cc908d95f2
Harshal Patel <harshal29379@gmail.com>
2025-06-21 23:10:40 -0400

    lengthOfLongestSubstring

--- Sliding Window/lengthOfLongestSubstring.py
+++ Sliding Window/lengthOfLongestSubstring.py
@@ -3,7 +3,15 @@ def lengthOfLongestSubstring(s):
     :type s: str
     :rtype: int
     """
-    
+    longest = 0
+    hashmap = {}
+    l = 0
+    for r in range(len(s)):
+        if s[r] in hashmap and hashmap[s[r]] >= l:
+            l = hashmap[s[r]] + 1
+        hashmap[s[r]] = r
+        longest = max(longest, r - l + 1)
+    return longest
 
 
 print(lengthOfLongestSubstring("abcabcbb")) # Outputs 3
