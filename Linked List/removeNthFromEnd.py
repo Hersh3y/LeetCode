6274cb324f0d5158ef135a4981810b23a85e8cca
Harshal Patel <harshal29379@gmail.com>
2025-06-28 04:03:45 -0400

    removeNthFromEnd

--- Linked List/removeNthFromEnd.py
+++ Linked List/removeNthFromEnd.py
@@ -10,4 +10,24 @@ class Solution(object):
         :type n: int
         :rtype: Optional[ListNode]
         """
-        
+        if head.next == None:
+            return None
+        length = 0
+        current = head
+        while current:
+            length += 1
+            current = current.next
+        counter = 0
+        prev = None
+        current = head
+        while current:
+            counter += 1
+            if counter == (length - n + 1):
+                if prev:
+                    prev.next = current.next
+                else:
+                    head = head.next
+            else:
+                prev = current
+                current = current.next
+        return head
