15a76d68edc9cfce8b6371243697ce5733694203
Harshal Patel <harshal29379@gmail.com>
2025-06-28 04:14:28 -0400

    validAnagram

--- Arrays and Hashing/validAnagram.py
+++ Arrays and Hashing/validAnagram.py
@@ -4,7 +4,20 @@ def isAnagram(s, t):
     :type t: str
     :rtype: bool
     """
-    
+    if len(s) != len(t):
+        return False
+    hashmap = {}
+    for i in s:
+        if i not in hashmap:
+            hashmap[i] = 0
+        hashmap[i] += 1
+    for i in t:
+        if i not in hashmap:
+            return False
+        hashmap[i] -= 1
+        if hashmap[i] < 0:
+            return False
+    return True
 
 
 print(isAnagram("anagram", "nagaram")) # Outputs True
