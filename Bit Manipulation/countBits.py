06e6e04e8a25c6d5144e1b1c07741491e914db6d
Harshal Patel <harshal29379@gmail.com>
2025-08-03 02:48:48 -0400

    countBits fix

--- Bit Manipulation/countBits.py
+++ Bit Manipulation/countBits.py
@@ -5,10 +5,10 @@ def countBits(n):
     """
     result = [0] * (n + 1)
     for i in range(1, n + 1):
-        result[i] = (i >> 1) + (i & 1)
+        result[i] = result[i >> 1] + (i & 1)
     return result
 
 
 print(countBits(2)) # Output: [0, 1, 1]
-print(countBits(5)) # Output: [0, 1, 1, 2, 2, 3]
-print(countBits(4)) # Output: [0, 1, 1, 2, 2]
+print(countBits(5)) # Output: [0, 1, 1, 2, 1, 2]
+print(countBits(4)) # Output: [0, 1, 1, 2, 1]
